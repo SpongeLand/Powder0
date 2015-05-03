82de9b77e78529704c697cc3588552d2309fb5b6
spongecattle <zayne8889@gmail.com>
2015-05-03 11:15:25 +0100

    Create powderframe.bat

---
+++ PowderFrame/powderframe.bat
@@ -0,0 +1,9 @@
+echo PowderFrame 1.0
+echo Finding Operating System... [Success]
+echo Booting Windows PowderFrame... [Success]
+echo OXO... [Success]
+echo YAYAYAYAYYAYAYAYAYAYAYYAYA... [Failure]
+echo Finding New Artifacts [Failure: No JENKINS project]
+pause
+echo YYYYYYYYYYYYYYYYYYYYYYYYYYY RGYFJ  YJHUGUYFHDTJ6UHYTR   [DECODING]
+echo WOOOOOOOOOOOOOOOOOOOOOOOOOP YAYAY  POOOOOOOOOOOOOOTIS   [DECODED]
